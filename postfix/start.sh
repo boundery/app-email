ba2b40b89051fef98f1c489317fc31b8b2164f75
Nolan Leake <nolan@sigbus.net>
2019-03-20 14:54:14 -0700

    Add submission container.

--- postfix/start.sh
+++ postfix/start.sh
@@ -5,6 +5,14 @@ useradd $USERNAME
 #XXX Tighten up inet_interfaces in main.cf, so we only listen on the public v4/6 addresses.
 echo "$DOMAINNAME" > /etc/mailname
 sed -i -e "s/XXXHOSTNAMEXXX/$HOSTNAME/g" -e "s/XXXDOMAINNAMEXXX/$DOMAINNAME/g" /etc/postfix/main.cf
+if [ -n "$SMTP" ]; then
+    cp /etc/postfix/master.cf.smtp /etc/postfix/master.cf
+elif [ -n "$SUBMISSION" ]; then
+    cp /etc/postfix/master.cf.submission /etc/postfix/master.cf
+else
+    echo "Neither SMTP nor SUBMISSION is set!" >&2
+    exit 99
+fi
 
 #XXX /etc/aliases root->user
 
