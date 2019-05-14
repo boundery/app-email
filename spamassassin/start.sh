3c76b463e58025f41d272e859900069c7abad12d
Nolan Leake <nolan@sigbus.net>
2019-05-14 14:38:21 -0700

    Add a note about a logged failure

--- spamassassin/start.sh
+++ spamassassin/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#XXX cannot create tmp lockfile /var/cache/spampd/bayes.lock.spamassassin.12 for /var/cache/spampd/bayes.lock: Permission denied
+
 PF_IP=`ip -o addr show to ${post_spam_SUBNET} | cut -d'/' -f1 | cut -d' ' -f7`
 echo $PF_IP
 spampd --host=$PF_IP:24 --relayhost=email-dovecot:24 \
